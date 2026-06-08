69140c47f10a1c0bc92030e2b587cba96096689f
Andi Muchamad Mugni Pabilla <144541746+Andimugni27@users.noreply.github.com>
2026-06-08 10:49:33 +0700

    Update modelling.py

--- modelling.py
+++ modelling.py
@@ -18,7 +18,7 @@ X = pd.get_dummies(X, drop_first=True)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # 3. Konfigurasi MLflow Lokal (Wajib diarahkan ke localhost untuk tingkat Basic)
-mlflow.set_tracking_uri("http://127.0.0.1:5001")
+#mlflow.set_tracking_uri("http://127.0.0.1:5001")
 mlflow.set_experiment("Eksperimen_Rekam_Medis")
 
 # Mengaktifkan autolog untuk mencatat metrik secara otomatis
